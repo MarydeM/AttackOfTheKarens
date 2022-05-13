ee40bb3070af634d2d655ec353582ede88567378
MarydeM <maryevelyndemarigny@gmail.com>
2022-05-13 15:07:14 -0500

    added character class

---
+++ AttackOfTheKarens/KarenLogic/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace KarenLogic {
+	/// <summary>
+	/// A class for creating the owner and managers
+	/// </summary>
+	public class Character {
+		public int Damage { get; set; }
+		public string Name { get; set; }
+		public PictureBox pic;
+        public int xLocation;
+		public int yLocation;
+		public Character(PictureBox pic) {
+			this.pic = pic;
+			this.pic.Visible = true;
+			this.pic.BringToFront();
+		}
+
+	}
+}
